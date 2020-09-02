2fc860a7073f84c11fddb9d5c2c21e342ad34b69
Maianne Ribeiro <mai628r@gmail.com>
2020-09-02 08:47:40 -0300

    feat(app): initial code setup

---
+++ src/components/app/index.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render(): JSX.Element {
+    return <h1>This is the main App component.</h1>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.join(__dirname, 'src', 'index.html'),
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'inline-source-map',
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'index.bundle.js',
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components/'),
+    },
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              localsConvention: 'camelCase',
+              modules: {
+                localIdentName: '[local]__[hash:base64:5]',
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000,
+              name: 'images/[name]__[hash].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [HtmlWebpackPluginConfig],
+};
