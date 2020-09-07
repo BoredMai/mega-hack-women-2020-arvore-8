82f924d5e806e413b13b0c17d9e1e5ec6dc67236
Maianne Ribeiro <mai628r@gmail.com>
2020-09-07 13:09:26 -0300

    feat(app): add initial gh-pages setup

--- webpack.config.js
+++ webpack.config.js
@@ -7,58 +7,63 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   inject: 'body',
 });
 
-module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  devtool: 'inline-source-map',
-  entry: path.join(__dirname, 'src', 'index.tsx'),
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'index.bundle.js',
-  },
-  resolve: {
-    alias: {
-      components: path.resolve(__dirname, 'src/components/'),
-      pages: path.resolve(__dirname, 'src/pages/'),
-      styles: path.resolve(__dirname, 'src/styles/'),
+module.exports = (_, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: argv.mode || 'development',
+    devtool: 'inline-source-map',
+    entry: path.join(__dirname, 'src', 'index.tsx'),
+    output: {
+      path: path.join(__dirname, 'public'),
+      publicPath: isProduction ? '/mega-hack-women-2020-arvore-8/' : '/',
+      filename: 'index.bundle.js',
     },
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-  },
-  module: {
-    rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: {
-                exportLocalsConvention: 'camelCase',
-                localIdentName: '[local]__[hash:base64:5]',
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src/components/'),
+        pages: path.resolve(__dirname, 'src/pages/'),
+        styles: path.resolve(__dirname, 'src/styles/'),
+      },
+      extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    },
+    module: {
+      rules: [
+        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+        { test: /\.tsx?$/, loader: 'ts-loader' },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: {
+                  exportLocalsConvention: 'camelCase',
+                  localIdentName: '[local]__[hash:base64:5]',
+                },
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.(png|jp(e*)g|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              limit: 8000,
-              name: 'images/[name]__[hash].[ext]',
+          ],
+        },
+        {
+          test: /\.(png|jp(e*)g|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                limit: 8000,
+                name: 'images/[name]__[hash].[ext]',
+              },
             },
-          },
-        ],
-      },
-    ],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [HtmlWebpackPluginConfig],
+          ],
+        },
+      ],
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+    plugins: [HtmlWebpackPluginConfig],
+  };
 };
